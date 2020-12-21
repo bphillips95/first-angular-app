595cec318ed93cc6a12e3ea5e98f96ae42bc1fed
Baruch Phillips <baruchphillips@gmail.com>
2020-12-21 16:15:34 -0500

    notes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -7,7 +7,10 @@ import {Hero} from '../hero'
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
+
+  //  using Input makes hero available to be used in the parent component ie. HeroesComponent
   @Input() hero: Hero;
+
   constructor() { }
 
   ngOnInit(): void {
